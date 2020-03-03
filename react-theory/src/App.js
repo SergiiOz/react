b801506fccb51885b0a0e0b5dcf32539b0a7ae75
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-03-03 23:58:16 +0200

    added inline style to jsx

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const divStyle = {
+    'color': 'red',
+  }
+
   return (
-    <div className="App">
-      <h1>
+    <div className="App" style={{ fontStyle: 'italic', fontSize: '30px' }}>
+      <h1 style={divStyle}>
         I have done it :).
       </h1>
-    </div>
+    </div >
 
   );
   //what is it JSX, how convert (important - when we use jsx we have to import 'REACT' from 'react')
