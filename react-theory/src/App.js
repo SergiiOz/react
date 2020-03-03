a99b6c4360a9d65d63071e58e29c13aca221a287
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-03-03 23:28:16 +0200

    how work jsx and convert data

---
+++ react-theory/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        I have done it :).
+      </h1>
+    </div>
+
+  );
+  //what is it JSX, how convert (important - when we use jsx we have to import 'REACT' from 'react')
+  // return (React.createElement(
+  //   'div',
+  //   {
+  //     className: 'App'
+  //   },
+  //   React.createElement(
+  //     'h1',
+  //     null,
+  //     'hello world'
+  //   )
+  // )
+  // )
+}
+
+export default App;
