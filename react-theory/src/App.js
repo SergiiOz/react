565480e61ef1f041ddfbe3a307a5497f0893b0e1
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-03-05 23:08:19 +0200

    adde componet and content transfer

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import Car from "./components/Car";
+
 function App() {
   const divStyle = {
     'color': 'red',
@@ -11,10 +13,29 @@ function App() {
       <h1 style={divStyle}>
         I have done it :).
       </h1>
+
+      {/* this is props.name props.year */}
+      <Car name={'Ford Mustang'} year={2018}>
+        {/* this props.children */}
+        <p style={{ color: 'blue' }}>Color</p>
+      </Car>
+
+      <Car name={'Audi'} year={2017} />
+
+      <Car name={'Mazda 3'} year={2010}>
+        <p style={{ color: 'green' }}>Color</p>
+      </Car>
+
     </div >
 
   );
   //what is it JSX, how convert (important - when we use jsx we have to import 'REACT' from 'react')
+  // example:
+  // <div class=App>
+  //   <h1>I have done it</h1>
+  // </div>
+  //we will write without jsx:
+
   // return (React.createElement(
   //   'div',
   //   {
@@ -23,7 +44,7 @@ function App() {
   //   React.createElement(
   //     'h1',
   //     null,
-  //     'hello world'
+  //     'I have done it'
   //   )
   // )
   // )
---
+++ react-theory/src/components/Car.js
@@ -0,0 +1,41 @@
+import React from 'react';
+// props.childern this is 'item'
+// <Car > <p>item</p><Car/>
+
+export default (props) => (
+    <div>
+        <h3>Car name: {props.name}</h3>
+        <span>Year: <strong>{props.year}</strong></span>
+        {props.children}
+    </div>
+);
+
+// version create component
+//version 1
+// function car() {
+//     return (
+//         <div>
+//             <p>This is car</p>
+//         </div>
+//     );
+// }
+
+//version 2
+// const car = () => {
+//     return (
+//         <div>
+//             <p>This is car</p>
+//         </div>
+//     );
+// }
+
+// version 3
+// const car = () => (
+//     <div>
+//         <p>This is car</p>
+//         <span>Mustang</span>
+//     </div>
+// )
+
+// export default car;
+
