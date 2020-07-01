5c8c6596705fd500f59889b5d83a7dad47ae5d59
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-07-01 07:28:47 +0300

    added redux form and changed Login component

--- social-network/src/components/Login/Login.js
+++ social-network/src/components/Login/Login.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder="Login" name="login" component="input" />
+      </div>
+      <div>
+        <Field placeholder="Password" name="password" component="input" />
+      </div>
+      <div>
+        <Field type="checkbox" name="rememberMe" component="input" /> remember
+        me
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm({
+  // a unique name for the form
+  form: 'login',
+})(LoginForm);
 
 const Login = () => {
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
   return (
     <div>
       <h1>LOGIN PAGE</h1>
       <p>Please auth on server</p>
+
+      <LoginReduxForm onSubmit={onSubmit} />
     </div>
   );
 };
--- social-network/src/redux/redux-store.js
+++ social-network/src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sideBarReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 let rootReducer = combineReducers({
   // our state
@@ -13,6 +14,7 @@ let rootReducer = combineReducers({
   sideBar: sideBarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 //added applyMiddleware with thunkMiddleware for work with thunk creators in reducer
