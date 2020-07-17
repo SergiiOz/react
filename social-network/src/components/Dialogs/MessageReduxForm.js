b3f8e929cfe3458c9f8f104ead2715b5bd9899a7
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-07-17 07:25:31 +0300

    added validation to PostReduxForm in  MyPost

--- social-network/src/components/Dialogs/MessageReduxForm.js
+++ social-network/src/components/Dialogs/MessageReduxForm.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import {
+  requiredField,
+  maxLengthCreator,
+} from '../utils/validators/validators';
+import { Textarea } from '../common/FormsControls/FormsControls';
+
+const maxLenght20 = maxLengthCreator(20);
 
 const MessageForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        <Field name="newMessageText" component="textarea" rows="5" cols="33" />
+        <Field
+          name="newMessageText"
+          // replacing string 'textarea' with component {Textarea} from components/common/FormsControls
+          component={Textarea}
+          validate={[requiredField, maxLenght20]}
+          rows="5"
+          cols="33"
+        />
       </div>
 
       <div>
