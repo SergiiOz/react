aac5d2f215b806ada5be0c5102f39e7f0847fc10
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-08-03 07:08:47 +0300

    added check file to UploadForm

--- photo-gallery/src/components/UploadForm.js
+++ photo-gallery/src/components/UploadForm.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const types = ['image/jpeg', 'image/png'];
+
+  const changeHandler = (event) => {
+    const selectedFile = event.target.files[0];
+    console.log(selectedFile);
+
+    if (selectedFile && types.includes(selectedFile.type)) {
+      setFile(selectedFile);
+      setErrorMessage(null);
+    } else {
+      setFile(null);
+      setErrorMessage('Please select an image file (png or jpeg');
+    }
+  };
+
   return (
     <form>
-      <input type="file" />
+      <input type="file" onChange={changeHandler} />
+
+      <div className="output">
+        {errorMessage && <div className="error">{errorMessage}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
     </form>
   );
 };
